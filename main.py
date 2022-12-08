94242e638469660b6cf210fed0c56cc567f9af93
Mehmet Ugur Gueral <mehmet-ugur.gueral@deutschebahn.com>
2022-12-08 14:28:55 +0100

    initial commit

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ client.setup_logging()
 
 @app.route("/")
 def index():
-    return f"AVI Aircraft Sensor test, Aviatar, id={SENSOR_ID}"
+    return f"AVI Aircraft Sensor Ugur 08-12-14:28, Aviatar, id={SENSOR_ID}"
 
 
 @app.route("/send_report")
